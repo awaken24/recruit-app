be688c628fb5e5bec6440dbecfe58553d0245b13
Ryan <ryanfelipe.rr@gmail.com>
2025-03-27 22:58:33 -0300

    Status da candidatura

---
+++ app/Enums/StatusCandidatura.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+class StatusCandidatura {
+    const ANDAMENTO = 'em andamento';
+    const PROCESSANDO = 'processando';
+    const PENDENTE = 'pendente';
+    const APROVADO = 'aprovado';
+    const REPROVADO = 'reprovado';
+}
